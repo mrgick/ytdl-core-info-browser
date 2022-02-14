41256077907a05034fdfd986b366ac36a1a10997
mrgick <45306973+mrgick@users.noreply.github.com>
2022-02-14 20:47:55 +0300

    fix CamelCase

--- yt_bundle/ytpl.ts
+++ yt_bundle/ytpl.ts
@@ -3,12 +3,12 @@ const realMiniget = require('miniget');
 // We import the library so it cached before using proxyquire
 const ytpl = require('ytpl');
 
-interface ytplBrowserOptions {
+interface YtplBrowserOptions {
     proxyUrl: string; // Ex: 'https://cors-anywhere.herokuapp.com/'
     proxyquireStubs?: any;
 }
 
-module.exports = (options: ytplBrowserOptions) => {
+module.exports = (options: YtplBrowserOptions) => {
     return proxyquire('ytpl', {
         miniget(url, opts) {
             return realMiniget(options.proxyUrl + url, opts);
